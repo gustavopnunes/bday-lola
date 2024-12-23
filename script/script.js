1208104d9e33887e331b52f36e580dfd2f2fd9e5
Gustavo Nunes <gustavo.nunes@ifood.com.br>
2024-12-23 00:45:22 -0300

    add testimonial author photos and more adjustments

--- script/script.js
+++ script/script.js
@@ -4,11 +4,11 @@ const messages = [
     "Ia mandar uma mensagem, mas resolvi fazer algo mais legal",
     "Porque você merece mais do que um simples <span class='highlight'>PARABÉNS!</span>",
     "Então, fui atrás de algumas pessoas pra me ajudarem nisso aqui...",
-    '<span class = "testimony"> Feliz aniversário amiga!!! Fico muito feliz de ter você por perto novamente, você sabe que sempre foi e sempre vai ser a minha melhor amiga, te amo muito, sempre vou querer o melhor pra você sempre!!! <br /> Se cuida e meus Parabéns, te desejo muita paz, amor, alegria, sucesso, saúde, que Deus continue abençoando sua vida sempre!!! “All the crazy shit I did tonight those are be the best memories” (trecho da nossa música de adolescência) <br />Amo todos os momentos que vivemos e a nossa adolescência que foi a “the best” agora já deu hahahah mo sempre queria estar contigo nessa data e nunca vou superar aquela vez que fez uma festa de aniversário na sua casa do pontal (na adolescência tb) e não pude ir 🥹🥹🥹🥹</span><span class="friend-name">Andressa</span>',
-    '<span class = "testimony">Hoje é o dia da pessoa que ganhei de presente nesse ano de 2024 e que o tempo (mesmo que pouco) a transformou em irmã! Você é mais que especial para mim, é aquela que compartilha risadas, conquistas, até os segredos mais bobos, e nossas viagens que vão sair certeza !!!! Sua amizade é um presente que a vida me deu, e eu sou eternamente grata por tê-la ao meu lado. <br /> Que o seu aniversário seja tão incrível quanto o coração que você tem! Desejo amor, saúde, alegria e que todos os seus sonhos se realizem. Mega parabéns pelo seu dia, que possa aproveitar cada minuto da melhor forma possível ! Te amo de montão ❤️</span><span class = "friend-name">Maria Fernanda</span>',
-    '<span class = "testimony">Pah, que dia especial hoje! <br /><br />Quero te desejar um Feliz Aniversário repleto de saúde, paz, amor, bençãos e muitas realizações! <br /><br />Que seus projetos tomem forma e que a felicidade seja uma constante em sua vida!! <br /><br /> Você sabe o quanto é especial e amada por nós! Aproveite seu dia! <br /><br />Te amamos!!!</span><span class = "friend-name">Tio Beto e Tio Lucas</span>',
-    '<span class = "testimony">"A gente não faz amigos, reconhece-os." <br /> <br />Hoje é dia de celebrar a vida dessa preciosidade que de “pequena” só tem o nome! A mulher mais resiliente que conheço, serena e de alma leve, sendo forjada por Deus para um grande propósito. Sou testemunha de absolutamente todo o seu empenho e transformação… quanta conquista em tão pouco tempo! E sabemos que é só o começo, né? <br /> <br /> Que o seu dia seja leve e divertido, como a vida deve ser! É uma honra ter você como amiga. Conte sempre comigo!</span><span class = "friend-name">Juliana</span>',
-    '<span class = "testimony">Depoimento Ayeska</span><span class = "friend-name">Ayeska</span>',
+    '<div class="testimony"><img src="images/andressa-photo.jpg" alt="Andressa"><span>Feliz aniversário amiga!!! Fico muito feliz de ter você por perto novamente, você sabe que sempre foi e sempre vai ser a minha melhor amiga, te amo muito, sempre vou querer o melhor pra você sempre!!! <br /> Se cuida e meus Parabéns, te desejo muita paz, amor, alegria, sucesso, saúde, que Deus continue abençoando sua vida sempre!!! “All the crazy shit I did tonight those are be the best memories” (trecho da nossa música de adolescência) <br />Amo todos os momentos que vivemos e a nossa adolescência que foi a “the best” agora já deu hahahah mo sempre queria estar contigo nessa data e nunca vou superar aquela vez que fez uma festa de aniversário na sua casa do pontal (na adolescência tb) e não pude ir 🥹🥹🥹🥹</span><span class="friend-name">Andressa</span></div>',
+    '<div class="testimony"><img src="images/mariaf-photo.jpg" alt="Maria Fernanda"><span>Hoje é o dia da pessoa que ganhei de presente nesse ano de 2024 e que o tempo (mesmo que pouco) a transformou em irmã! Você é mais que especial para mim, é aquela que compartilha risadas, conquistas, até os segredos mais bobos, e nossas viagens que vão sair certeza !!!! Sua amizade é um presente que a vida me deu, e eu sou eternamente grata por tê-la ao meu lado. <br /> Que o seu aniversário seja tão incrível quanto o coração que você tem! Desejo amor, saúde, alegria e que todos os seus sonhos se realizem. Mega parabéns pelo seu dia, que possa aproveitar cada minuto da melhor forma possível ! Te amo de montão ❤️</span><span class = "friend-name">Maria Fernanda</span></div>',
+    '<div class="testimony"><img src="images/adalberto-photo.jpeg" alt="Tio Beto e Tio Lucas"><span>Pah, que dia especial hoje! <br />Quero te desejar um Feliz Aniversário repleto de saúde, paz, amor, bençãos e muitas realizações! <br />Que seus projetos tomem forma e que a felicidade seja uma constante em sua vida!! <br /> Você sabe o quanto é especial e amada por nós! Aproveite seu dia! <br /><br />Te amamos!!!</span><span class = "friend-name">Tio Beto e Tio Lucas</span></div>',
+    '<div class="testimony"><img src="images/juliana-photo.jpeg" alt="Juliana"><span>"A gente não faz amigos, reconhece-os." <br /> <br />Hoje é dia de celebrar a vida dessa preciosidade que de “pequena” só tem o nome! A mulher mais resiliente que conheço, serena e de alma leve, sendo forjada por Deus para um grande propósito. Sou testemunha de absolutamente todo o seu empenho e transformação… quanta conquista em tão pouco tempo! E sabemos que é só o começo, né? <br /> <br /> Que o seu dia seja leve e divertido, como a vida deve ser! É uma honra ter você como amiga. Conte sempre comigo!</span><span class = "friend-name">Juliana</span></div>',
+    '<div class="testimony"><img src="images/ayesha.jpg" alt="Ayeska"><span>Depoimento Ayeska</span><span class = "friend-name">Ayeska</span></div>',
     "Aproveita seu dia! E que ele seja tão especial quanto você <3"
 ];
 
@@ -20,30 +20,29 @@ function showNextMessage() {
     const carouselElement = document.querySelector('.carousel');
     const closeButton = document.getElementById('closeButton');
     const nextButton = document.getElementById('nextButton');
-    messageElement.style.opacity = 0; // Iniciar a transição
+    messageElement.style.opacity = 0; 
 
     setTimeout(() => {
         currentIndex = (currentIndex + 1) % messages.length;
         messageElement.innerHTML = messages[currentIndex];
-        messageElement.style.opacity = 1; // Finalizar a transição
+        messageElement.style.opacity = 1;
 
-        // Ocultar a foto da aniversariante para a mensagem específica
         if (currentIndex === 5 || currentIndex === 6 || currentIndex === 7 || currentIndex === 8 || currentIndex === 9) {
             photoElement.style.display = 'none';
         } else {
             photoElement.style.display = 'block';
         }
 
-        // Exibir o carrossel de imagens para a última mensagem
+    
         if (currentIndex === messages.length - 1) {
             carouselElement.style.display = 'flex';
-            nextButton.style.display = 'none'; // Ocultar o botão "Avançar"
+            nextButton.style.display = 'none'; 
         } else {
             carouselElement.style.display = 'none';
-            closeButton.style.display = 'none'; // Ocultar o botão de fechar
-            nextButton.style.display = 'block'; // Mostrar o botão "Avançar"
+            closeButton.style.display = 'none';
+            nextButton.style.display = 'block';
         }
-    }, 500); // Tempo da transição deve coincidir com o CSS
+    }, 500); 
 }
 
 function enlargeImage(event) {
